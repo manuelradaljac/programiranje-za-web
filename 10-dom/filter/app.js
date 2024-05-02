ef4adeb7ada4e74b8c982a7be3f949b33f775309
“Manuel <manuelradaljac@gmail.com>
2024-05-02 13:30:50 +0200

    kdf98as893hbf3jufn

--- 10-dom/filter/app.js
+++ 10-dom/filter/app.js
@@ -1,4 +1,31 @@
 function selectFilter(btn) {
     document.querySelector(".mjmne").innerHTML = btn.innerHTML;
-    btn.classList.toggle("filter-button-selected");   
+    btn.classList.toggle("filter-button-selected");
+}
+
+function onIphoneClicked() {
+    const btn = document.querySelector(".iphone-btn")
+    if (btn.classList.contains("filter-btn-clicked") === false) {
+        btn.classList.add("filter-btn-clicked")
+    } else {
+        btn.classList.remove("filter-btn-clicked")
+    }
+}
+
+function onSamsungClicked() {
+    const btn = document.querySelector(".samsung-btn")
+    if (btn.classList.contains("filter-btn-clicked") === false) {
+        btn.classList.add("filter-btn-clicked")
+    } else {
+        btn.classList.remove("filter-btn-clicked")
+    }
+}
+
+function onXiaomiClicked() {
+    const btn = document.querySelector(".xiaomi-btn")
+    if (btn.classList.contains("filter-btn-clicked") === false) {
+        btn.classList.add("filter-btn-clicked")
+    } else {
+        btn.classList.remove("filter-btn-clicked")
+    }
 }
